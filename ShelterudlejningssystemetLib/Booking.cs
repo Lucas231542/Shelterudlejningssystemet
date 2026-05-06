b32698687e7373578a9ffe13f6a79d6921f7ade9
Sofyane Amrhar <soa016@edu.zealand.dk>
2026-05-07 00:04:21 +0200

    Tilføjer TilføjBooking, SletBooking, RedigerBooking og HentKommendeBookinger metoder til Booking klasse

--- ShelterudlejningssystemetLib/Booking.cs
+++ ShelterudlejningssystemetLib/Booking.cs
@@ -7,22 +7,26 @@ namespace ShelterudlejningssystemetLib
     public class Booking
     {
         // Private variabler 
-        
+
         private int _bookingId;
         private DateTime _startDato;
         private DateTime _slutDato;
         private int _antalMennesker;
-        
+
+        // liste over alle bookinger 
+
+        private static List<Booking> _alleBookinger = new List<Booking>();
+
         // Konstruktør 
         public Booking()
         {
             _bookingId = 0;
             _startDato = DateTime.Now;
-            _slutDato  = DateTime.Now;
+            _slutDato = DateTime.Now;
             _antalMennesker = 0;
         }
         // Konstruktør  
-        
+
         public Booking(int bookingId, DateTime startDato, DateTime slutDato, int antalMennesker)
         {
             _bookingId = bookingId;
@@ -35,14 +39,16 @@ namespace ShelterudlejningssystemetLib
         public int BookingId
         {
             get { return _bookingId; }
-            set {_bookingId = value; }
+            set { _bookingId = value; }
         }
-        public DateTime StartDato   
+
+        public DateTime StartDato
         {
-            get { return _startDato; }  
+            get { return _startDato; }
             set { _startDato = value; }
         }
-        public DateTime SlutDato 
+
+        public DateTime SlutDato
         {
             get { return _slutDato; }
             set
@@ -51,7 +57,8 @@ namespace ShelterudlejningssystemetLib
                 {
                     throw new ArgumentException("SlutDato cannot be before StartDato.");
                 }
-                _slutDato  = value;
+
+                _slutDato = value;
             }
         }
 
@@ -69,18 +76,58 @@ namespace ShelterudlejningssystemetLib
             }
         }
 
+        
+        // Returnerer en liste over bookinger der endnu ikke er startet
+        public static List<Booking> HentKommendeBookinger()
+        {
+            List<Booking> kommendeBookinger = new List<Booking>();
+            foreach (Booking b in _alleBookinger)
+            {
+                if (b.StartDato > DateTime.Now)
+                {
+                    kommendeBookinger.Add(b);
+                }
+            }
+
+            return kommendeBookinger;
+        }
+
+
+        // Tilføjer en booking til listen over alle bookinger 
+        public static void TilføjBooking(Booking booking)
+        {
+            _alleBookinger.Add(booking);
+        } 
+        
+        
+        
+        // Sletter en booking fra listen baseret på bookingId 
+        public static void SletBooking(int bookingId)
+        {
+            _alleBookinger.RemoveAll(b => b.BookingId == bookingId);
+        }
+
+        
+        // Redigere en eksisterende booking med nye værdier 
+        public static void RedigerBooking(int bookingId, DateTime nyStartDato, DateTime nySlutDato,
+            int nyAntalMennekser)
+        {
+            foreach (Booking b in _alleBookinger)
+            {
+                if (b.BookingId == bookingId)
+                {
+                    b.StartDato = nyStartDato;
+                    b.SlutDato = nySlutDato;
+                    b.AntalMennesker = nyAntalMennekser;
+                }
+            }
+        }
+
         // Metoder
         public override string ToString()
         {
-            return $"BookingId: {BookingId}, StartDato: {StartDato}, SlutDato: {SlutDato}, AntalMennesker: {AntalMennesker}";
-        }
+            return
+                $"BookingId: {BookingId}, StartDato: {StartDato}, SlutDato: {SlutDato}, AntalMennesker: {AntalMennesker}";
         }
     }
-
-
-  
-
-
-
-
-
+}
