134d8863f8a7b1f365597239faa60f4000b15ad5
Anton Alfred Vilken Olesen <ano010@edu.zealand.dk>
2026-05-06 12:46:19 +0200

    shelter start

--- ShelterudlejningssystemetLib/FDF_Shelter.cs
+++ ShelterudlejningssystemetLib/FDF_Shelter.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace ShelterudlejningssystemetLib
+    // Anton Shelter
 {
     public class FDF_Shelter
     {
+        
+        // Instansfelter
+        private int _shelterId;
+        private string _shelterNavn;
+        private int _lokation;
+        private int _størrelse;
+    
+
+        // Konstruktør
+        public FDF_Shelter()
+        {
+            ShelterId = 0;
+            ShelterNavn = "";
+            Lokation = 0;
+            Størrelse = 0;
+        }
+
+        public FDF_Shelter(int shelterid, string shelternavn, int lokation, int størrelse)
+        {
+            ShelterId = shelterid;
+            ShelterNavn = shelternavn;
+            Lokation = lokation;
+            Størrelse = størrelse;
+
+        }
+
+        //properties
     }
 }
+
+
