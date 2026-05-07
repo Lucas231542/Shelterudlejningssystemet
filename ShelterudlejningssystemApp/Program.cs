bc88eea82f26a2b65996c154865a26eddfeb7cc6
Anton Alfred Vilken Olesen <ano010@edu.zealand.dk>
2026-05-07 13:19:14 +0200

    list

--- ShelterudlejningssystemApp/Program.cs
+++ ShelterudlejningssystemApp/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ShelterudlejningssystemetLib;
+
+Console.WriteLine("Hello, World!");
+
+List<FDF_Shelter> shelters = new List<FDF_Shelter>();
+
+shelters.Add(new FDF_Shelter(1, "numero uno", "Ganløse", 5, 3660, true));
+shelters.Add(new FDF_Shelter(2, "numero dos", "Ganløse", 5, 3660, false));
+
