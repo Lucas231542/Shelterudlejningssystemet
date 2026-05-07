4730b5aaf52bbfd4870d297cae79c19c902acb9f
AHMAD-ui-coder <ahm002@edu.zealand.dk>
2026-05-07 09:54:02 +0200

    Blog klassen opdateret

--- ShelterudlejningssystemetLib/Blog opslag.cs
+++ ShelterudlejningssystemetLib/Blog opslag.cs
@@ -6,10 +6,69 @@ namespace ShelterudlejningssystemetLib
 {
     public class Blog_opslag
     {
-        
-    }
+        // instans felter
 
+        private int _InlægID;
+        private string _Titel;
+        private string _Tekst;
+        private DateTime _Dato;
+
+        // konstruktører
+
+        public Blog_opslag()
+
+        {
+            _InlægID = 0;
+            _Titel = "";
+            _Tekst = "";
+            _Dato = DateTime.Now;
+
+        }
+
+        public Blog_opslag(int InlægID, string Titel, string Tekst, DateTime Dato)
+        {
+            _InlægID = InlægID;
+            _Titel = Titel;
+            _Tekst = Tekst;
+            _Dato = Dato;
+
+        }
+
+        // propeerties
+        public int InlægID
+        {
+            get { return _InlægID; }
+            set { _InlægID = value; }
+        }
+        public string Titel
+        {
+            get { return _Titel; }
+            set { _Titel = value; }
+        }
+        public string Tekst
+        {
+            get { return _Tekst; }
+            set { _Tekst = value; }
+        }
+        public DateTime Dato
+        {
+            get { return _Dato; }
+            set { _Dato = value; }
+        }
+
+
+        // metoder 
+        public override string ToString()
+        {
+            return "InlægID: InlægID= " + InlægID + ", Titel= " + Titel + ", Tekst= " + Tekst +
+                ", Dato= " + Dato;
+        }
+
+
+    }
 }
+
+
     
         
 
