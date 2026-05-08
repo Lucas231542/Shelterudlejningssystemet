045a2377e968e55f2bb69722fd7aa1e3b2adfb9d
Lucas Bacher-Nielsen <lub003@edu.zealand.dk>
2026-05-08 14:20:12 +0200

    razor, men virker ikke endnu

--- ShelterRazor/Program.cs
+++ ShelterRazor/Program.cs
@@ -9,8 +9,8 @@ bool TestData = true;
 List<Blog_opslag> Blog_opslagListe = new List<Blog_opslag>();
 if (TestData)
 {
-    Blog_opslag opslag1 = new Blog_opslag(1, "Blog opslag 1", "Dette er det første blog opslag", DateTime.Now);
-    Blog_opslag opslag2 = new Blog_opslag(2, "Blog opslag 2", "Dette er det andet blog opslag", DateTime.Now);
+    Blog_opslag opslag1 = new Blog_opslag(1, "Blog opslag 1", "Dette er det første blog opslag",new DateTime(2026, 8, 8));
+    Blog_opslag opslag2 = new Blog_opslag(2, "Blog opslag 2", "Dette er det andet blog opslag", new DateTime(2027, 3, 20));
     Blog_opslag opslag3 = new Blog_opslag(3, "Blog opslag 3", "Dette er det tredje blog opslag", DateTime.Now);
 
     Blog_opslagListe.Add(opslag1);
@@ -19,8 +19,7 @@ if (TestData)
 }
 
 
-
-builder.Services.AddSingleton<Blog_opslag>(new Blog_opslag(Blog_opslagListe));
+builder.Services.AddSingleton<BlogListe>(new BlogListe(Blog_opslagListe));
 
 var app = builder.Build();
 
