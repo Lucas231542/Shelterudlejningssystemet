32df1b5e8be9f435d957881d63ad73f363eba2d6
Lucas Bacher-Nielsen <lub003@edu.zealand.dk>
2026-05-11 10:30:49 +0200

    blogliste slette metoder

--- ShelterudlejningssystemetLib/BlogListe.cs
+++ ShelterudlejningssystemetLib/BlogListe.cs
@@ -26,54 +26,6 @@ namespace ShelterudlejningssystemetLib
             set { _begivenheder = value; }
         }
 
-        // KredsLeder tilføje en begivenhed til kredsens blog
-        public void AddEvent(Blog_opslag NewEvent)
-        {
-            _begivenheder.Add(NewEvent);
-        }
-
-        // KredsLeder slette en begivenhed fra kredsens blog
-        public void RemoveEvent(int EventToRemove)
-        {
-            Blog_opslag begivenhed = GetEvent(EventToRemove);
-            if (begivenhed != null)
-            {
-                _begivenheder.Remove(begivenhed);
-            }
-
-        }
-        // KredsLeder hente alle begivenheder fra kredsens blog
-        public List<Blog_opslag> ListAll()
-        {
-            return new List<Blog_opslag>(_begivenheder);
-        }
-
-        // KredsLeder hente en begivenhed fra kredsens blog
-        public Blog_opslag GetEvent(int BlogId)
-        {
-            Blog_opslag resBegivenhed = null; // Return null if not found
-            foreach (Blog_opslag b in _begivenheder)
-            {
-                if (b.Id == BlogId)
-                {
-                    return b; // Return the found event
-                }
-            }
-            return resBegivenhed;
-        }
-
-        // opdatere en begivenhed i kredsens blog
-        public Blog_opslag UpdateBegivenhed(int BlogId, Blog_opslag updatedBegivenhed)
-        {
-            Blog_opslag begivenhed = GetEvent(BlogId);
-            if (begivenhed != null)
-            {
-                begivenhed.Id = updatedBegivenhed.Id;
-                begivenhed.Titel = updatedBegivenhed.Titel;
-                begivenhed.Tekst = updatedBegivenhed.Tekst;
-                begivenhed.Dato = updatedBegivenhed.Dato;
-            }
-            return begivenhed;
-        }
+        // tilføj alle metoder under her
     }
 }
