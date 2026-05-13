36a1dbdc60206795c920923960ee0bbb3c57633d
Aleksandar Nakic <aln007@edu.zealand.dk>
2026-05-13 19:09:20 +0200

    l

---
+++ ShelterRazor/Pages/FdfShelter/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShelterRazor.Pages.FdfShelter
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
